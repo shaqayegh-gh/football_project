907b8c7a09805f79a6420569dcc18cfa21070f63
shaqayegh-gh <ghorbanpoor.shaghayegh@gmail.com>
2020-11-20 06:53:10 -0800

    shaghayegh has sent fourth file.

--- team.py
+++ team.py
@@ -20,28 +20,26 @@ class Team (Ball,Robot):
         self.forward.manage()
         self.goaler.manage()
 
-
-
     def Calculate_goal(self):
         # Calculate team goals
         if self.color == "red":
-            if 900 <= self.ball_pos[0] <= 930 & 240 <= self.ball_pos[1] <= 390:
+            if self.ball_pos[0] == 30.5 * 30 and 8.5 * 30 <= self.ball_pos[1] <= 12.5 * 30:
                 self.goal += 1
                 logging.basicConfig(filename='footbaler_robot_log.log', format='%(asctime)s %(message)s')
                 logging.info('The red team scored a goal')
                 self.full_pos = [(10.5 * 30, 3.5 * 30), (20.5 * 30, 3.5 * 30), (10.5 * 30, 17.5 * 30),
-                            (20.5 * 30, 17.5 * 30),
-                            (18.5 * 30, 10.5 * 30), (12.5 * 30, 10.5 * 30), (45, 10.5 * 30),
-                            (29.5 * 30, 10.5 * 30)]
+                                 (20.5 * 30, 17.5 * 30),
+                                 (18.5 * 30, 10.5 * 30), (12.5 * 30, 10.5 * 30), (45, 10.5 * 30),
+                                 (29.5 * 30, 10.5 * 30)]
 
         if self.color == "blue":
-            if 0 <= self.ball_pos[0] <= 30 & 240 <= self.ball_pos[1] <= 390:
+            if self.ball_pos[0] == 15 and 8.5 * 30 <= self.ball_pos[1] <= 12.5 * 30:
                 self.goal += 1
                 logging.basicConfig(filename='footbaler_robot_log.log', filemode='w', format='%(asctime)s %(message)s')
                 logging.info("The blue team scored a goal")
                 self.full_pos = [(10.5 * 30, 3.5 * 30), (20.5 * 30, 3.5 * 30), (10.5 * 30, 17.5 * 30),
-                            (20.5 * 30, 17.5 * 30),
-                            (18.5 * 30, 10.5 * 30), (12.5 * 30, 10.5 * 30), (45, 10.5 * 30),
-                            (29.5 * 30, 10.5 * 30)]
+                                 (20.5 * 30, 17.5 * 30),
+                                 (18.5 * 30, 10.5 * 30), (12.5 * 30, 10.5 * 30), (45, 10.5 * 30),
+                                 (29.5 * 30, 10.5 * 30)]
 
 

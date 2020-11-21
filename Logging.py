fa42d96ce0a0ef91635b0df8fb0d82ec135956e9
z-najafiyan <213.najafiyan@gmail.com>
2020-11-20 18:28:35 -0800

    logging

---
+++ Logging.py
@@ -0,0 +1,16 @@
+import logging
+
+logging.basicConfig(filename='Robot_footballer.log', level=logging.INFO ,format='%(asctime)s _ %(levelname)s _ %(message)s')
+logging.info('The red team scored a goal')
+
+
+logging.basicConfig(filename='Robot_footballer.log', level=logging.INFO ,format='%(asctime)s _ %(levelname)s _ %(message)s')
+logging.info('The blue team scored a goal')
+
+
+
+logging.basicConfig(filename='Robot_footballer.log', level=logging.INFO ,format='%(asctime)s _ %(levelname)s _ %(message)s')
+logging.info('A robot was removed')
+
+
+
